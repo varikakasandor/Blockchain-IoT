febc055c80ad2d9b279c31d972f4630096152c10
varik <varikakasandor@gmail.com>
2021-05-13 20:03:26 +0200

    Minor code change

--- plots.py
+++ plots.py
@@ -25,9 +25,6 @@ def create_availability_plot(benchmark_m=2, benchmark_n=3):
     plt.plot(x, gen_y(log_availability(lambda n:1),x),label='1 out of n',marker="o")
     plt.plot(x, gen_y(log_availability(lambda n:math.floor(n/2)+1),x),label='n/2+1 out of n',marker="v")
     plt.plot(x, gen_y(log_availability(find_linear(benchmark_m=benchmark_m,benchmark_n=benchmark_n)),x),label=f'{benchmark_m} out of {benchmark_n} level',marker="x") #Approximately n-log(n)
-    #plt.plot(x, gen_y(log_availability(lambda n:n),x),label='n out of n',marker="s")
-    #for (m,n) in [(1,1),(1,3),(1,5),(2,3),(3,5)]:
-    #    plt.scatter(n,log_availability(lambda _:m)(n))
     plt.xlim([np.min(x)-1,np.max(x)+1])
     plt.ylim(bottom=0)
     plt.grid(color='lightgrey')
